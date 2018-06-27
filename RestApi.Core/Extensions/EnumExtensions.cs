fe7564bd40de35a43984e49ab603ccd16954aecb
KimKyengJun <juniair.kim@gmail.com>
2018-06-28 00:27:37 +0900

    확장 메소드 추가

---
+++ RestApi.Core/Extensions/EnumExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace RestApi.Core.Extensions
+{
+    /// <summary>
+    /// Extension methods for <see cref="Enum"/>
+    /// </summary>
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// Convert <see cref="Enum"/> to <see cref="string"/> for <see cref="DescriptionAttribute"/> of <see cref="Enum"/> 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string GetDescription(this Enum value)
+        {
+            // get attributes  
+            var field = value.GetType().GetField(value.ToString());
+            var attributes = field.GetCustomAttributes(typeof(DescriptionAttribute), false) as DescriptionAttribute[];
+            // Description is in a hidden Attribute class called DisplayAttribute
+            // Not to be confused with DisplayNameAttribute
+            dynamic displayAttribute = null;
+
+            if (attributes.Any())
+            {
+                displayAttribute = attributes.ElementAt(0);
+            }
+
+            // return description
+            return displayAttribute?.Description ?? value.ToString();
+        }
+
+        /// <summary>
+        /// Convert <see cref="String"/> to <see cref="Enum"/> for <see cref="DescriptionAttribute"/> of <see cref="Enum"/> 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="description"></param>
+        /// <returns></returns>
+        public static T GetValue<T>(this string description)
+        {
+            MemberInfo[] fis = typeof(T).GetFields();
+
+            foreach (var fi in fis)
+            {
+                DescriptionAttribute[] attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+                if (attributes != null && attributes.Length > 0 && attributes[0].Description == description)
+                    return (T)Enum.Parse(typeof(T), fi.Name);
+            }
+
+            throw new Exception("Not found");
+        }
+    }
+}
---
+++ RestApi.Core/Extensions/StringExtenstions.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestApi.Core.Extensions
+{
+    /// <summary>
+    /// Extension methods for String
+    /// </summary>
+    public static class StringExtensions
+    {
+
+
+        /// <summary>
+        /// Indicates wether the specified string is <see cref="null"/> or an <see cref="String.Empty"/> string.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>true if the value parameter is null or an empty string (""); otherwise, false.</returns>
+        public static bool IsNullOrEmpty(this string value)
+        {
+            return string.IsNullOrEmpty(value);
+        }
+
+
+        /// <summary>
+        /// Indicates wether the specified string is <see cref="null"/>, <see cref="String.Empty"/> or 
+        /// consists only of white-space characters.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>true if the value parameter is <see cref="null"/> or <see cref="String.Empty"/>,
+        /// or if value consists excuusively of white-spcace characters.</returns>
+        public static bool IsNullOrWhiteSpace(this string value)
+        {
+            return string.IsNullOrWhiteSpace(value);
+        }
+
+        /// <summary>
+        /// Convert <see cref="string"/> to <see cref="bool"/>.
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="predicate"><paramref name="value"/> condition <see cref="predicate{T}"/>.</param>
+        /// <returns></returns>
+        public static bool ConvertToBoolean(this string value, Predicate<string> predicate)
+        {
+            if (predicate == null)
+            {
+                predicate = o => o.ToUpper().Equals("Y");
+            }
+            return predicate(value);
+        }
+
+
+        public static DateTime ConvertToDateTime(this string value, Func<string, DateTime> func = null)
+        {
+            if (func == null)
+            {
+                func = str =>
+                {
+                    str = str.Replace(".", "").Replace("-", "");
+                    var length = str.Length;
+                    var day = str.Substring(length - 2, 2);
+                    var month = str.Substring(length - 4, 2);
+                    var year = str.Substring(0, length - 4);
+                    return DateTime.Parse($@"{year} {month} {day}");
+                };
+            }
+
+            return func(value);
+        }
+    }
+}
