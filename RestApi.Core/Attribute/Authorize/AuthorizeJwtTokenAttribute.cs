cc8e7b7753fd801f6b47eab7ecb07850de46b531
KimKyengJun <juniair.kim@gmail.com>
2018-06-28 00:28:15 +0900

    Atrribute 클래스 추가

---
+++ RestApi.Core/Attribute/Authorize/AuthorizeJwtTokenAttribute.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestApi.Core.Attribute.Authorize
+{
+    /// <summary>
+    /// The authorization policy for token-based authentication
+    /// </summary>
+    public class AuthorizeJwtTokenAttribute : AuthorizeAttribute
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public AuthorizeJwtTokenAttribute()
+        {
+            // Add the JWT bearer authentication scheme
+            AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme;
+        }
+
+        #endregion
+    }
+}
