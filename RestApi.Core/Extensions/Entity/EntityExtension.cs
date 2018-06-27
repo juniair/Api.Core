b33e74f573201a1acb0afcf44b8164f8e515ee99
KimKyengJun <juniair.kim@gmail.com>
2018-06-28 00:27:48 +0900

    DB 용 확장 메소드 추가

---
+++ RestApi.Core/Extensions/Entity/EntityExtension.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace RestApi.Core.Extensions.Entity
+{
+    public static class EntityExtension
+    {
+        public static ModelBuilder PropertyConverter<TEntity, TValue>(this ModelBuilder modelBuilder, Expression<Func<TEntity, TValue>> expression, ValueConverter converter) where TEntity : class
+        {
+            modelBuilder.Entity<TEntity>()
+                .Property(expression)
+                .HasConversion(converter);
+
+            return modelBuilder;
+        }
+    }
+}
